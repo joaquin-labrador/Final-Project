a7b26227737f2d034951ec15706221c95ed97704
joaquin <labradordeveloper@gmail.com>
2022-05-27 18:17:17 -0300

    Added user options

--- src/Jframes/UserInterface.java
+++ src/Jframes/UserInterface.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 public class UserInterface extends JFrame {
     public UserInterface() throws HeadlessException {
-
+        this.setSize(500,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import Jframes.UserInterface;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Joaquin branch");
+System.out.println("Joaquin branch");
+
+        UserInterface appWindow = new UserInterface();
+        appWindow.setTitle();
+        appWindow.setVisible(true);
+
     }
 }
