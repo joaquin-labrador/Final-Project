3253b961db180592c37c2934cedcc06e81fc2234
Guntergs <santii001@hotmail.com>
2022-05-27 18:51:23 -0300

    host y server

---
+++ src/Employee/Host.java
@@ -0,0 +1,14 @@
+package project_classes;
+
+public class Host extends Employee{
+
+
+    private double hourSalary;
+
+    private double tips;
+
+    public Host(String name, String surName, LocalDate dateOfBrith, Integer id) {
+        super(name,surName, dateOfBrith,id);
+    }
+
+}
---
+++ src/Employee/Server.java
@@ -0,0 +1,15 @@
+package project_classes;
+
+
+public class Server extends Employee{
+
+    private double hourSalary;
+
+    private double tips;
+
+    public Server(String name, String surName, LocalDate dateOfBrith, Integer id) {
+        super(name,surName, dateOfBrith,id);
+    }
+
+
+}
