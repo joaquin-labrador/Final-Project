da6fa82585999d5aae379cee07aebc28a1db2432
joaquin <labradordeveloper@gmail.com>
2022-05-30 10:51:40 -0300

    Add getters and setters

--- src/Employee/Host.java
+++ src/Employee/Host.java
@@ -1,14 +1,44 @@
 package project_classes;
 
-public class Host extends Employee{
+import Employee.Employee;
+
+import java.time.LocalDate;
+
+public class Host extends Employee {
 
 
     private double hourSalary;
 
     private double tips;
 
-    public Host(String name, String surName, LocalDate dateOfBrith, Integer id) {
-        super(name,surName, dateOfBrith,id);
+    public Host() {
     }
 
+    public Host(String name, String lastName, LocalDate dateOfBrith, Integer id, String userName, String password, double hourSalary, double tips) {
+        super(name, lastName, dateOfBrith, id, userName, password);
+        this.hourSalary = hourSalary;
+        this.tips = tips;
+    }
+
+
+    public double getHourSalary() {
+        return hourSalary;
+    }
+
+    public void setHourSalary(double hourSalary) {
+        this.hourSalary = hourSalary;
+    }
+
+    public double getTips() {
+        return tips;
+    }
+
+    public void setTips(double tips) {
+        this.tips = tips;
+    }
+
+    @Override
+    protected void finishDay() {
+
+    }
 }
