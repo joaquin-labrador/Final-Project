547dcaf375a879a4ba02847d6c29f51c9f114e69
joaquin <labradordeveloper@gmail.com>
2022-05-30 10:57:52 -0300

    Add getters and setters

--- src/Employee/Manager.java
+++ src/Employee/Manager.java
@@ -1,11 +1,37 @@
 package Employee;
 
+import java.time.LocalDate;
+
 public class Manager extends Employee{
     private Double hourSalary;
     private Boolean inEmergency = false;
 
 
+    public Manager() {
 
+    }
+
+    public Manager(String name, String lastName, LocalDate dateOfBrith, Integer id, String userName, String password, Double hourSalary, Boolean inEmergency) {
+        super(name, lastName, dateOfBrith, id, userName, password);
+        this.hourSalary = hourSalary;
+        this.inEmergency = inEmergency;
+    }
+
+    public Double getHourSalary() {
+        return hourSalary;
+    }
+
+    public void setHourSalary(Double hourSalary) {
+        this.hourSalary = hourSalary;
+    }
+
+    public Boolean getInEmergency() {
+        return inEmergency;
+    }
+
+    public void setInEmergency(Boolean inEmergency) {
+        this.inEmergency = inEmergency;
+    }
 
     @Override
     protected void finishDay() {
