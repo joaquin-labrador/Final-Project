b09884b62dd7acee6d80fb5b0ca23d7d535f42b4
joaquin <labradordeveloper@gmail.com>
2022-05-30 22:28:51 -0300

    Create JSON for test

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,57 @@
 import Employee.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<Employee> employeeList = new ArrayList<>();
+
+        Chef chef1 = new Chef("Joaquin", "Labrador", LocalDate.of(1998, 11, 6).toString(),
+                41458511, "chefJoaquin", "123gf45", Salarys.CHEF.getSalary());
+        Kitchener kitchener1 = new Kitchener("Matias", "Aceval", LocalDate.of(2001, 12, 12).toString(), 43567654,
+                "kitchenerMatias", "67sdafdf@-3", Salarys.KITCHENER.getSalary());
+        Manager manager1 = new Manager("Maximo", "Torterolo", LocalDate.of(2002, 6, 23).toString(),
+                44533543, "managerMaximo", "34324esds3", Salarys.SERVER.getSalary());
+        Host host1 = new Host("Santiago","Pavon", LocalDate.of(2002,4,23).toString(),
+                43578956,"hostSantiago","23123423Hb2343",Salarys.HOST.getSalary());
+        Server server1 = new Server("Agustin","Gomes",LocalDate.of(1998,9,29).toString(),41234234,
+                "serverAgustin","@dfd678",Salarys.SERVER.getSalary());
+        Server server2 = new Server("Matias","Fierro", LocalDate.of(1996,2,23).toString(),39455959,
+                "serverMatias", "ebfe@45", Salarys.SERVER.getSalary());
+
+        employeeList.add(chef1);
+        employeeList.add(kitchener1);
+        employeeList.add(manager1);
+        employeeList.add(host1);
+        employeeList.add(server1);
+        employeeList.add(server2);
 
 
-    }
-
-
-    /*
-    public static void escribirArchivoReservas(File fileReservas, ArrayList<Reserva> listaReserva) {
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileReservas));
+        try{
+            BufferedWriter bufferedWriter = new BufferedWriter((new FileWriter("Employees.json")));
             Gson gson = new GsonBuilder()
                     .setPrettyPrinting()
-                    .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
                     .create();
-
-
-            Type listOfReservaObject = new TypeToken<ArrayList<Reserva>>() {
-            }.getType();
-            gson.toJson(listaReserva, listOfReservaObject, bufferedWriter);
-
+            Type listOfEmployeeFile = new TypeToken<ArrayList<Employee>>() {}.getType();
+            gson.toJson(employeeList,listOfEmployeeFile,bufferedWriter);
             bufferedWriter.close();
-
-        } catch (IOException e) {
-            System.out.println("ERROR al Escribir el archivo: " + e.getMessage());
+        }catch (IOException e){
+            System.out.println("Read file error");
             e.printStackTrace();
         }
+
+
+
     }
-    ///
-     */
-    /// -------------------------------------------------- DESERIALIZACION RESERVAS --------------------------------------------------
-    /*
-    public static ArrayList<Reserva> leerArchivoReservas (File fileReservas) {
-        ArrayList<Reserva> listaReserva = new ArrayList<Reserva>();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileReservas));
 
-            Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new JsonDeserializer<LocalDate>() {
-                @Override
-                public LocalDate deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-                    return LocalDate.parse(json.getAsJsonPrimitive().getAsString());
-                }
-            }).create();
-
-            Type listOfReservaObject = new TypeToken<ArrayList<Reserva>>() {
-            }.getType();
-
-            listaReserva=(gson.fromJson(bufferedReader, listOfReservaObject))
-
-    */
 }
