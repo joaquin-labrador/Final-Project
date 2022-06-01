5bb4b30eb890160bb799b468a261751de624cdb9
joaquin <labradordeveloper@gmail.com>
2022-06-01 12:50:35 -0300

    Add and Create JSONFiles of Resto

--- src/Restaurant/Lounge.java
+++ src/Restaurant/Lounge.java
@@ -9,14 +9,16 @@ public class Lounge {
     private List<Employee> employees = new ArrayList<>();
     private List<Table> tables = new ArrayList<>();
     private List<Menu> menu = new ArrayList<>();
+    private List<Beverages> beverages = new ArrayList<>();
 
     public Lounge() {
     }
 
-    public Lounge(List<Employee> employees, List<Table> tables, List<Menu> menu) {
+    public Lounge(List<Employee> employees, List<Table> tables, List<Menu> menu, List<Beverages> beverages) {
         this.employees = employees;
         this.tables = tables;
         this.menu = menu;
+        this.beverages = beverages;
     }
 
     public List<Employee> getEmployees() {
@@ -39,7 +41,27 @@ public class Lounge {
         return menu;
     }
 
+
+
     public void setMenu(List<Menu> menu) {
         this.menu = menu;
+    }
+
+    public List<Beverages> getBeverages() {
+        return beverages;
+    }
+
+    public void setBeverages(List<Beverages> beverages) {
+        this.beverages = beverages;
+    }
+
+    @Override
+    public String toString() {
+        return "Lounge{" +
+                "employees=" + employees +
+                ", tables=" + tables +
+                ", menu=" + menu +
+                ", beverages=" + beverages +
+                '}';
     }
 }
