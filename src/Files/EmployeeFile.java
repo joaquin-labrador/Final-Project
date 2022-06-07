3bc6f0df555fd22ecef0bc2c1d65afbbeca86993
joaquin <labradordeveloper@gmail.com>
2022-06-07 18:54:26 -0300

    Add the manager options : to add new employee with gson

--- src/Files/EmployeeFile.java
+++ src/Files/EmployeeFile.java
@@ -14,6 +14,8 @@ import java.util.List;
 public class EmployeeFile implements FilesOperations {
     List<Employee> employeeList = new ArrayList<>();
 
+    WriteNewEmployees writeNewEmployees = new WriteNewEmployees();
+
     public EmployeeFile() throws IOException {
         readFile();
     }
@@ -93,138 +95,42 @@ public class EmployeeFile implements FilesOperations {
 
         if (employeeToSave instanceof Manager manager) {
             //Write to file
-            try {
-                reader = new BufferedReader(new FileReader("JSONFiles\\Manager.json"));
-                managerListAux = gson.fromJson(reader, (new TypeToken<List<Manager>>() {
-                }.getType()));
-                managerListAux.add(manager);
-                exit = new BufferedWriter(new FileWriter("JSONFiles\\Manager.json"));
-                String json = gson.toJson(managerListAux, managerListAux.getClass());
-                exit.write(json);
-                System.out.println("Manager saved");
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println(managerListAux.toString());
-                assert exit != null;
-                try {
-                    exit.close();
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            writeNewEmployees.addManager(reader, exit, gson, manager, managerListAux);
+
         } else if (employeeToSave instanceof Chef chef) {
-               //Write to file
-            try {
-                reader = new BufferedReader(new FileReader("JSONFiles\\Chef.json"));
-                chefListAux = gson.fromJson(reader, (new TypeToken<List<Chef>>() {
-                }.getType()));
-                chefListAux.add(chef);
-                exit = new BufferedWriter(new FileWriter("JSONFiles\\Chef.json"));
-                String json = gson.toJson(chefListAux, chefListAux.getClass());
-                exit.write(json);
-                System.out.println("Chef saved");
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println(chefListAux.toString());
-                assert exit != null;
-                try {
-                    exit.close();
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            //Write to file
+            writeNewEmployees.addChef(reader, exit, gson, chef, chefListAux);
 
 
         } else if (employeeToSave instanceof Kitchener Kitchener) {
             //Write to file
-            try {
-                reader = new BufferedReader(new FileReader("JSONFiles\\Kitchener.json"));
-                kitchenerListAux = gson.fromJson(reader, (new TypeToken<List<Kitchener>>() {
-                }.getType()));
-                kitchenerListAux.add(Kitchener);
-                exit = new BufferedWriter(new FileWriter("JSONFiles\\Kitchener.json"));
-                String json = gson.toJson(kitchenerListAux, kitchenerListAux.getClass());
-                exit.write(json);
-                System.out.println("Kitchener saved");
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println(kitchenerListAux.toString());
-                assert exit != null;
-                try {
-                    exit.close();
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            writeNewEmployees.addKitchener(reader, exit, gson, Kitchener, kitchenerListAux);
+
         } else if (employeeToSave instanceof Server server) {
             //Save server with gson
-            try {
-                reader = new BufferedReader(new FileReader("JSONFiles\\Servers.json"));
-                serverListAux = gson.fromJson(reader, (new TypeToken<List<Server>>() {
-                }.getType()));
-                serverListAux.add(server);
-                exit = new BufferedWriter(new FileWriter("JSONFiles\\Servers.json"));
-                String json = gson.toJson(serverListAux, serverListAux.getClass());
-                exit.write(json);
-                System.out.println("Server saved");
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println(serverListAux.toString());
-                assert exit != null;
-                try {
-                    exit.close();
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        } else if (employeeToSave instanceof  Host host) {
+            writeNewEmployees.addServer(reader, exit, gson, server, serverListAux);
+
+        } else if (employeeToSave instanceof Host host) {
             //Save host with gson
+            writeNewEmployees.addHost(reader, exit, gson, host, hostListAux);
+
+        }
+    }
+
+        @Override
+        public void showFile () {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            BufferedReader reader = null;
             try {
-                reader = new BufferedReader(new FileReader("JSONFiles\\Host.json"));
-                hostListAux = gson.fromJson(reader, (new TypeToken<List<Host>>() {
-                }.getType()));
-                hostListAux.add(host);
-                exit = new BufferedWriter(new FileWriter("JSONFiles\\Host.json"));
-                String json = gson.toJson(hostListAux, hostListAux.getClass());
-                exit.write(json);
-                System.out.println("Host saved");
+                reader = new BufferedReader(new FileReader("JSONFiles\\Manager.json"));
+                reader.toString();
+
             } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                System.out.println(hostListAux.toString());
-                assert exit != null;
-                try {
-                    exit.close();
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
         }
-    }
 
-    @Override
-    public void showFile() {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader("JSONFiles\\Manager.json"));
-            reader.toString();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        public List<Employee> getEmployeeList () {
+            return employeeList;
         }
     }
-
-    public List<Employee> getEmployeeList() {
-        return employeeList;
-    }
-}
---
+++ src/Files/WriteNewEmployees.java
@@ -0,0 +1,132 @@
+package Files;
+
+import Employee.*;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.util.List;
+
+public class WriteNewEmployees {
+
+    public WriteNewEmployees() {
+    }
+
+    public void addManager(BufferedReader reader, BufferedWriter exit, Gson gson, Manager manager , List<Manager> managerListAux) {
+        try {
+            reader = new BufferedReader(new FileReader("JSONFiles\\Manager.json"));
+            managerListAux = gson.fromJson(reader, (new TypeToken<List<Manager>>() {
+            }.getType()));
+            managerListAux.add(manager);
+            exit = new BufferedWriter(new FileWriter("JSONFiles\\Manager.json"));
+            String json = gson.toJson(managerListAux, managerListAux.getClass());
+            exit.write(json);
+            System.out.println("Manager saved");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(managerListAux.toString());
+            assert exit != null;
+            try {
+                exit.close();
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void addChef (BufferedReader reader, BufferedWriter exit, Gson gson, Chef chef , List<Chef> chefListAux) {
+        try {
+            reader = new BufferedReader(new FileReader("JSONFiles\\Chef.json"));
+            chefListAux = gson.fromJson(reader, (new TypeToken<List<Chef>>() {
+            }.getType()));
+            chefListAux.add(chef);
+            exit = new BufferedWriter(new FileWriter("JSONFiles\\Chef.json"));
+            String json = gson.toJson(chefListAux, chefListAux.getClass());
+            exit.write(json);
+            System.out.println("Chef saved");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(chefListAux.toString());
+            assert exit != null;
+            try {
+                exit.close();
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void addKitchener (BufferedReader reader, BufferedWriter exit, Gson gson, Kitchener kitchener , List<Kitchener> kitchenerListAux) {
+        try {
+            reader = new BufferedReader(new FileReader("JSONFiles\\Kitchener.json"));
+            kitchenerListAux = gson.fromJson(reader, (new TypeToken<List<Kitchener>>() {
+            }.getType()));
+            kitchenerListAux.add(kitchener);
+            exit = new BufferedWriter(new FileWriter("JSONFiles\\Kitchener.json"));
+            String json = gson.toJson(kitchenerListAux, kitchenerListAux.getClass());
+            exit.write(json);
+            System.out.println("Kitchener saved");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(kitchenerListAux.toString());
+            assert exit != null;
+            try {
+                exit.close();
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void addServer (BufferedReader reader, BufferedWriter exit, Gson gson, Server server , List<Server> serverListAux) {
+        try {
+            reader = new BufferedReader(new FileReader("JSONFiles\\Servers.json"));
+            serverListAux = gson.fromJson(reader, (new TypeToken<List<Server>>() {
+            }.getType()));
+            serverListAux.add(server);
+            exit = new BufferedWriter(new FileWriter("JSONFiles\\Servers.json"));
+            String json = gson.toJson(serverListAux, serverListAux.getClass());
+            exit.write(json);
+            System.out.println("Server saved");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(serverListAux.toString());
+            assert exit != null;
+            try {
+                exit.close();
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void addHost (BufferedReader reader, BufferedWriter exit, Gson gson, Host host , List<Host> hostListAux) {
+        try {
+            reader = new BufferedReader(new FileReader("JSONFiles\\Host.json"));
+            hostListAux = gson.fromJson(reader, (new TypeToken<List<Host>>() {
+            }.getType()));
+            hostListAux.add(host);
+            exit = new BufferedWriter(new FileWriter("JSONFiles\\Host.json"));
+            String json = gson.toJson(hostListAux, hostListAux.getClass());
+            exit.write(json);
+            System.out.println("Host saved");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(hostListAux.toString());
+            assert exit != null;
+            try {
+                exit.close();
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
