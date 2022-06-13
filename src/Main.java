eb229515f4ea5a76584c36cecf0cfc65cbc4c6ed
joaquin <labradordeveloper@gmail.com>
2022-06-13 00:28:12 -0300

    PreProyect finish

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,15 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         AppResto resto = new AppResto("La marquesita");
-       // System.out.println(resto.getRestoEmployee());
-       // System.out.println(resto.getBeveragesList());
-
-        Employee user;
-
-        user = resto.signIn("kitchenerMatias", "67sdafdf@-3");
-
-        resto.userOptions(user);
-
-
-
-
+        try {
+            Employee user;
+            user = resto.signIn("kitchenerMatias", "67sdafdf@-3");
+            resto.userOptions(user);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        System.out.println("Gracias por utilizar mi software");
+        System.out.println("By Joaquin Labrador");
 
 
     }
